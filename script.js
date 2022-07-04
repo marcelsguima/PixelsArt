8f98170abc742346d94a78fdd6c135eed9d63f80
marceslguima <marcelsguima@gmail.com>
2022-07-04 18:55:32 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-// Definir cor inicial
+let board = document.getElementById('pixel-board');
+let pixel = document.getElementsByClassName('pixel');
+let selected = 0;
 
-let selectedColor = document.getElementById("#color1");
-console.log(selectedColor);
+//Pixel Board
+for(let i = 0; i < 5; i += 1){
+    let line = document.createElement('div');
+    board.appendChild(line);
+    line.className = 'pixel';
 
+    for(let i = 0; i < 5; i += 1){
+        let colum = document.createElement('div');
+        board.appendChild(colum);
+        colum.className = 'pixel';
+    }
+}
+//Default Color
+
+window.onload = function () {
+    colorPalette[0].className += ' selected';
+  };
