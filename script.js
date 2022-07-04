3a171170183cc7a165537feacd9ca43dbd05f02f
marceslguima <marcelsguima@gmail.com>
2022-07-04 20:15:11 -0300

    requisito 6 correção

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let board = document.getElementById('pixel-board');
 let pixel = document.getElementsByClassName('pixel');
-let selected = 0;
+let selected = [];
+let colorPalette = [];
 
 //Pixel Board
-for(let i = 0; i < 5; i += 1){
+for(let i = 1; i < 5; i += 1){
     let line = document.createElement('div');
     board.appendChild(line);
     line.className = 'pixel';
@@ -14,8 +15,23 @@ for(let i = 0; i < 5; i += 1){
         colum.className = 'pixel';
     }
 }
+
 //Default Color
 
 window.onload = function () {
     colorPalette[0].className += ' selected';
   };
+
+//Select color
+
+function selectColor(){
+    let pixelAll = document.querySelectorAll('.pixel');
+    for(let i =0; i < pixelAll.length; i +=1){
+        pixelAll[i].addEventListener('click', (toPrint) => {
+        let nowColor = document.getElementsByClassName('selected');
+        let printColor = getComputedStyle(nowColor).backgroundColor;
+        toPrint.target.style.backgroundColor = nowColor;
+        });
+    }
+}
+window.addEventListener('click')
