b8227fcdbcd92d8330f0c930e0e484ff3fd75198
marceslguima <marcelsguima@gmail.com>
2022-07-06 22:32:37 -0300

    ---------

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let board = document.getElementById('pixel-board');
 let pixel = document.getElementsByClassName('pixel');
 let selected = [];
 let colorPalette = [];
+const findSelected = document.getElementsByClassName('selected');
 
 //Pixel Board
 for (let i = 1; i < 5; i += 1) {
@@ -16,21 +17,35 @@ for (let i = 1; i < 5; i += 1) {
   }
 }
 
-// //Default Color
-
 // //Select color
 
 let cores = document.querySelector('#color-palette').children;
 for (let i = 0; i < cores.length; i += 1) {
   cores[i].addEventListener('click', seleciona);
 }
-// function logcor(){
-//   console.log("pl");
-// }
-// }
 
 function seleciona(event) {
   let selecionada = document.getElementsByClassName('selected');
   selecionada[0].classList.remove('selected');
   event.target.classList.add('selected');
+}
+
+// Paint Pixel
+
+function pintaQuadro(event) {
+  event.target.classList.remove(event.target.classList[1]);
+  event.target.classList.add(findSelected[0].classList[1]);
+}
+
+//Clear button
+
+let botaoLimpa = document.getElementById('clear-board');
+botaoLimpa.addEventListener('click', limpa);
+
+function limpa() {
+  const pixelClass = document.getElementsByClassName('pixel');
+  for (let i = 0; i < pixelClass.length; i += 1) {
+    pixelClass[i].className = 'pixel';
+    pixelClass[i].addEventListener('click', pintaQuadro);
+  }
 }
